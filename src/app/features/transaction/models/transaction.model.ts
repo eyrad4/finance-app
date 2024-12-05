1086d0c9b5c3361d755345655853842287258bf8
Pavel Bozhok <pavel.bozhok@scaleo.io>
2024-12-05 11:13:57 +0100

    added transaction model

---
+++ src/app/features/transaction/models/transaction.model.ts
@@ -0,0 +1,11 @@
+import { ExpensesCategory, IncomeCategory, OperationType } from '@finance-app/domain/types';
+import { Nullish } from '@finance-app/shared/cdk/types';
+
+export interface TransactionModel {
+    id: string;
+    name: string;
+    amount: number;
+    category: ExpensesCategory | IncomeCategory | Nullish;
+    operationType: OperationType;
+    // date: Date;
+}
